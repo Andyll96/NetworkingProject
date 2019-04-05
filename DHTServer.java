75174c6ac7d36a761bda1d07dc8e951a5b61cf09
allactah <andy.llactahuamani@ryerson.ca>
2019-04-05 01:26:32 -0400

    created sockets and it waits for a message from the predecessor

--- DHTServer.java
+++ DHTServer.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -54,15 +55,21 @@ public class DHTServer {
 
         //Prints Variable Initialization 
         this.panel.DHTPrint("Initializing Server Variables...");
-        this.panel.DHTPrint("Server Info:\n" + "\tServer Port Number : " + this.serverPort + "\n\tServer ID: "
-                + this.serverID + "\n\tServer IP Address: " + serverIPAddress + "\n");
-        this.panel.DHTPrint("Successor Server Info:\n" + "\tSuccessor Server Port Number: " + this.successorServerPort
-                + "\n\tSuccessor Server ID: " + this.successorID + "\n\tSuccessor Server IP Address: "
-                + this.successorServerIP + "\n");
+        this.panel.DHTPrint("Server Info:\n" + "\tServer Port Number : " + this.serverPort + "\n\tServer ID: " + this.serverID + "\n\tServer IP Address: " + serverIPAddress + "\n");
+        this.panel.DHTPrint("Successor Server Info:\n" + "\tSuccessor Server Port Number: " + this.successorServerPort + "\n\tSuccessor Server ID: " + this.successorID + "\n\tSuccessor Server IP Address: " + this.successorServerIP + "\n");
 
+        try {
+            //essentially the welcome socket
+            tcpSocket = new ServerSocket(this.serverPort);
+            
+            //TODO: init the udpSocket if the Server ID = 1
+            initTcpThread();
+            //initUdpThread();
+        } catch (Exception e) {
+            panel.DHTPrint("Port Number Not Available");
+        }
         //Defines and starts Threads for TCP and UDP connections
-        initTcpThread();
-        initUdpThread();
+
     }
     
     private void initUdpThread() {
@@ -86,11 +93,30 @@ public class DHTServer {
 
             @Override
             public void run() {
-                try {
-                    panel.DHTPrint("TCP Thread Running\n");
-                } catch (Exception e) {
-                    panel.DHTPrint("Problem with the TCP Thread");
+                while (true) { //like Game Loop
+
+                    String message;
+                    try {
+                        panel.DHTPrint("TCP Thread Running");
+                        panel.DHTPrint("Waiting for message from predecessor on port " + serverPort);
+                        
+                        //listens for messages from the predecessor, creates and returns a socket that'll be used for communication 
+                        Socket connectionToPredecessor = tcpSocket.accept();
+                        panel.DHTPrint("Connected to " + connectionToPredecessor.getRemoteSocketAddress());
+
+                        //gets the input from the socket, the message from the predecessor, and saves the message/data
+                        DataInputStream data = new DataInputStream(connectionToPredecessor.getInputStream());
+                        message = data.readUTF();
+                        panel.DHTPrint("MESSAGE FROM PREDECESSOR SERVER: " + message);
+
+
+                        connectionToPredecessor.close();
+                    } catch (Exception e) {
+                        panel.DHTPrint("Problem with the TCP Thread");
+                    }
                 }
+
+                
             }
         });
         tcpThread.start();
