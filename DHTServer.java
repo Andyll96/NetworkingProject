e68b9d5da9c9117ac789ea3c1dd6b35211ea8d4f
allactah <andy.llactahuamani@ryerson.ca>
2019-04-05 16:31:58 -0400

    added some functionality to UDP and TCP, needs work!

--- DHTServer.java
+++ DHTServer.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -8,6 +9,7 @@ public class DHTServer {
     
     int serverPort;
     int serverID;
+
     int successorServerPort;
     String successorServerIP;
 
@@ -28,7 +30,8 @@ public class DHTServer {
 
     Panel panel;
 
-    public DHTServer(String serverPort, String serverID, String successorServerPort, String successorServerIP, Panel panel) {
+    public DHTServer(String serverPort, String serverID, String successorServerPort, String successorServerIP,
+            Panel panel) {
         this.panel = panel;
 
         //Initializes servers variables
@@ -55,32 +58,57 @@ public class DHTServer {
 
         //Prints Variable Initialization 
         this.panel.DHTPrint("Initializing Server Variables...");
-        this.panel.DHTPrint("Server Info:\n" + "\tServer Port Number : " + this.serverPort + "\n\tServer ID: " + this.serverID + "\n\tServer IP Address: " + serverIPAddress + "\n");
-        this.panel.DHTPrint("Successor Server Info:\n" + "\tSuccessor Server Port Number: " + this.successorServerPort + "\n\tSuccessor Server ID: " + this.successorID + "\n\tSuccessor Server IP Address: " + this.successorServerIP + "\n");
+        this.panel.DHTPrint("Server Info:\n" + "\tServer Port Number : " + this.serverPort + "\n\tServer ID: "
+                + this.serverID + "\n\tServer IP Address: " + serverIPAddress + "\n");
+        this.panel.DHTPrint("Successor Server Info:\n" + "\tSuccessor Server Port Number: " + this.successorServerPort
+                + "\n\tSuccessor Server ID: " + this.successorID + "\n\tSuccessor Server IP Address: "
+                + this.successorServerIP + "\n");
 
         try {
             //essentially the welcome socket
             tcpSocket = new ServerSocket(this.serverPort);
-            
+            if (this.serverID == 1) {
+                udpSocket = new DatagramSocket(this.serverPort);
+            }
+
             //TODO: init the udpSocket if the Server ID = 1
+            //Defines and starts Threads for TCP and UDP connections
+            initUdpThread();
             initTcpThread();
-            //initUdpThread();
         } catch (Exception e) {
             panel.DHTPrint("Port Number Not Available");
         }
-        //Defines and starts Threads for TCP and UDP connections
 
     }
     
+    //Creates a UDP Socket for communication with the client
     private void initUdpThread() {
         udpThread = new Thread(new Runnable(){
         
             @Override
             public void run() {
-                try {
-                    panel.DHTPrint("UDP Thread Running\n");
-                } catch (Exception e) {
-                    panel.DHTPrint("Problem with the UDP Thread");
+                while (true) {
+
+                    String clientServerMessage; //message from the client/Server
+                    byte[] clientDataBuffer = new byte[1024];
+
+                    try {
+                        panel.DHTPrint("UDP Thread Running\n");
+                        
+                        //Declares packet variable to store receieved Packet 
+                        DatagramPacket receivedPacket = new DatagramPacket(clientDataBuffer, clientDataBuffer.length);
+                        udpSocket.receive(receivedPacket); //receives and stores data in receivedPacket Variable
+
+                        clientServerMessage = new String(receivedPacket.getData()); //extracts message from UDP packet
+                        panel.DHTPrint("MESSAGE FROM CLIENT: " + clientServerMessage);
+                        
+                        if (clientServerMessage.contains("GET ALL IP")) {
+                            int uniquePortNumber = reserveUniqueUDPPort();
+                        }
+                        
+                    } catch (Exception e) {
+                        panel.DHTPrint("Error in the UDP Thread");
+                    }
                 }
             }
         });
@@ -88,6 +116,7 @@ public class DHTServer {
         panel.DHTPrint("Initializing UDP Thread...");
     }
 
+    
     private void initTcpThread() {
         tcpThread = new Thread(new Runnable() {
 
@@ -99,7 +128,7 @@ public class DHTServer {
                     try {
                         panel.DHTPrint("TCP Thread Running");
                         panel.DHTPrint("Waiting for message from predecessor on port " + serverPort);
-                        
+
                         //listens for messages from the predecessor, creates and returns a socket that'll be used for communication 
                         Socket connectionToPredecessor = tcpSocket.accept();
                         panel.DHTPrint("Connected to " + connectionToPredecessor.getRemoteSocketAddress());
@@ -107,19 +136,51 @@ public class DHTServer {
                         //gets the input from the socket, the message from the predecessor, and saves the message/data
                         DataInputStream data = new DataInputStream(connectionToPredecessor.getInputStream());
                         message = data.readUTF();
-                        panel.DHTPrint("MESSAGE FROM PREDECESSOR SERVER: " + message);
+                        panel.DHTPrint("RECEIVED MESSAGE FROM PREDECESSOR: " + message);
 
+                        if (message.contains("GET ALL IP") && serverID == 1) { //where server sends IP Addresses of all servers to the p2p client
+                            String listOfIPMessage = okStatus + message;
+                            panel.DHTPrint("MESSAGE TO CLIENT: " + listOfIPMessage);
+                            //sendToClient();
+
+                        } else if (message.contains("GET ALL IP")) { //you get a message to add your IP Address to the received message and send it to your successor.
+
+                        } else if (condition) {
+
+                        } else if (condition) {
+
+                        }
 
                         connectionToPredecessor.close();
                     } catch (Exception e) {
-                        panel.DHTPrint("Problem with the TCP Thread");
+                        panel.DHTPrint("Error in the TCP Thread");
                     }
                 }
 
-                
             }
         });
         tcpThread.start();
         panel.DHTPrint("Initializing TCP Thread...");
     }
+
+    //reserves an open UDP port for the client's reference
+    protected int reserveUniqueUDPPort() {
+        int testPortNum = serverPort + 1; //try the port after welcome port
+        boolean searching = true;
+        while (searching == true) {
+            try {
+                //We only want to test for availability, we don't want to keep open it YET
+                DatagramSocket newPort = new DatagramSocket(testPortNum);
+                searching = false;
+                newPort.close();
+                break;
+            } catch (Exception e) {
+                testPortNum++;
+            }
+        }
+		return testPortNum;
+	}
+    //used when you have all the server's IP addresses and you want to send them to the client
+	protected void sendToClient(String clientIP, int clientPort, String message) {
+	}
 }
