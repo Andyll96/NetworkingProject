0ac606cc34b2d459c0eb9d374da8cb8b1e4556c2
Danri <danri@Danris-MacBook-Pro-2.local>
2019-04-07 21:27:23 -0400

    Update HostServer.java

--- HostServer.java
+++ HostServer.java
@@ -5,5 +5,20 @@ import java.text.*;
 
 
 public class HostServer{
-     
+     int originPort;
+
+     final int httpStatusCode200 = 200; //Ok 
+     final int httpStatusCode400 = 400; //Bad request
+     final int httpStatusCode404 = 404; //Cannot be found
+     final int httpStatusCode505 = 505; //not supported
+
+     ServerSocket hostServerTCP;
+     Thread hostThread;
+    //method to initialize server
+    public HostServer(int portNum)
+    {
+        //originPort = portNum;
+        hostServerTCP = new ServerSocket(portNum);
+        hostThread = new Thread();
+    }
 }
