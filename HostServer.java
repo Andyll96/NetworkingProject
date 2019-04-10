3e764aaf4063bf5f262092108be7926c31ad4476
Danri <danri@Danris-MacBook-Pro-2.local>
2019-04-09 20:03:30 -0400

    update to hostserver

--- HostServer.java
+++ HostServer.java
@@ -16,8 +16,7 @@ public class HostServer{
      ServerSocket hostServerTCP;
      Thread hostThread;
      String TCPMessage;
-
-a
+     public static ArrayList<UniqueTCP> peerClientList = new ArrayList<UniqueTCP>();
 
     //method to initialize server
     public HostServer(int portNum, Panel panel)
@@ -27,7 +26,7 @@ a
         try {
             hostServerTCP = new ServerSocket(portNum);
             hostThread = new Thread();
-            hostThread.Start(TCPThread);
+            hostThread.start();
         } catch (Exception e) {
             //TODO: handle exception
             System.out.println("Not a valid port number.");
@@ -69,18 +68,98 @@ a
                 try {
                     clientSocket = hostServerTCP.accept();
                     inStream = new DataInputStream(clientSocket.getInputStream());
-                    scan = new Scanner(in.readUTF());
+                    scan = new Scanner(inStream.readUTF());
                     scan.next(); 
 
                     availablePort = nextAvailablePort();   
-                    System.out.println(statusCode200 + " " + availablePort);
+                    System.out.println(httpStatusCode200 + " " + availablePort);
                     outStream = new DataOutputStream(clientSocket.getOutputStream());
+                    outStream.writeUTF(inStream.readUTF());
                     clientSocket.close();
 
                 } catch (Exception e) {
                     //TODO: handle exception
                     System.out.println("Connection error!");
                 }
+            }
+        }
+    };
+
+    public class UniqueTCP
+    {
+        final int httpStatusCode200 = 200;  //OK
+        final int httpStatusCode301 = 301;  //Moved permanently
+        final int httpStatusCode400 = 400;  //Bad request
+        final int httpStatusCode404 = 404;  //HTTP not found
+        final int httpStatusCode505 = 505;  // HTTP version not supported
+
+        ServerSocket TCP;
+        Thread TCPThread;
+
+        public UniqueTCP(int portNum, Panel panel)
+        {
+            try
+            {
+                TCP = new ServerSocket(portNum);
+                TCPThread = new Thread()
+            } catch (Exception e){
+                //TODO: handle exception
+                System.out.println("Not a valid port number");
+            }
+        }
+
+        Runnable TCPConnectRun = new Runnable()
+        {
+            public void run()
+            {
+                String message, fileName, request, httpV, response, connection, cType, timeString, temp, temp2;
+                Socket tcpSocket;
+                DataInputStream inStream;
+                Scanner scan;
+                File file, badFile, file2;
+                try
+                {
+                    tcpSocket = TCP.accept();
+                    inStream = new DataInputStream(tcpSocket.getInputStream());
+                    message = inStream.readUTF();
+                    scan = new Scanner(inStream.readUTF());
+                    request = scan.next();
+
+                    if (request.equals("GET"))
+                    {
+                        fileName = scan.next();
+                        httpV = scan.next();
+                        connection = "CLOSED";
+                        cType = "image";
+
+                        if (httpV.equals("HTTP/1.1"))
+                        {
+                            fileName = fileName.substring(1);
+                            file = new File(fileName);
+                            try
+                            {
+                                temp = fileName.substring(0, fileName.indexOf(".jpeg"));
+                                temp2 = fileName.substring(0, fileName.indexOf(".jpeg"));
+                                temp += "---Trial---.jpeg"; //??
+                                badFile = new File(temp);
+                                badFile.createNewFile();
+                                badFile.delete();
+
+                                file2 = new File(temp2 + ".jpg");
+
+                                if (file2.exists())
+                                {
+                                    file = new File(temp2 + ".jpg");
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception e)
+                {
+                    System.out.println(e);
+
+                }
+
             }
         }
     }
